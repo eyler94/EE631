f9555ae36701f110d6436aa415540cc88ebf52f3
Michael Eyler <eyler94@gmail.com>
2020-01-13 23:51:42 -0700

    Still have questions on corner detection and have some bugs on line detection.

--- hw1/VideoFeed.py
+++ hw1/VideoFeed.py
@@ -4,62 +4,82 @@ import numpy as np
 import cv2
 import imutils
 
-vs = VideoStream(0).start()
-# vs = cv2.VideoCapture(0)
+# vs = VideoStream(0).start()
+vs = cv2.VideoCapture(0)
+vs.set(cv2.CAP_PROP_FPS, 15)
 # vs.set(cv.CAP_PROP_FPS, 15)
 setting = "o" # ordinary
 
 while True:
     if setting=="o": # Ordinary
-        frame = vs.read()
+        ret, frame = vs.read()
         frame = imutils.resize(frame, width=600)
         cv2.imshow("Eyler", frame)
         key = cv2.waitKey(1) & 0xFF
     elif setting=="t": # Thresholding
-        frame = vs.read()
+        ret, frame = vs.read()
         frame = imutils.resize(frame, width=600)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)[1]
-        # thresh = cv2.threshold(gray, 75, 255, cv2.THRESH_BINARY_INV)[1]
         cv2.imshow("Thresholding", thresh)
         key = cv2.waitKey(1) & 0xFF
     elif setting=="a": # Canny difference
-        frame = vs.read()
+        ret, frame = vs.read()
         frame = imutils.resize(frame, width=600)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         edge = cv2.Canny(gray, 30, 150)
         cv2.imshow("Canny (edge detection)", edge)
         key = cv2.waitKey(1) & 0xFF
-    elif setting=="i": # invert
-        frame = vs.read()
+    elif setting=="l": # line detection
+        ret, frame = vs.read()
         frame = imutils.resize(frame, width=600)
-        i_frame = frame#np.linalg.inv(frame)
-        cv2.imshow("Inverted frame", i_frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        edge = cv2.Canny(gray, 30, 150)
+        lines = cv2.HoughLines(edge, 1, np.pi/180, 200)
+        if lines is not None:
+            print("lines:", lines)
+            for line in lines:
+                for pixel, theta in line:
+                    dx = np.cos(theta)
+                    dy = np.sin(theta)
+                    x0 = dx*pixel
+                    y0 = dy*pixel
+                    x1 = int(x0 - 1000*dx) # Why -+, +-?
+                    y1 = int(y0 + 1000*dy)
+                    x2 = int(x0 + 1000*dx)
+                    y2 = int(y0 - 1000*dy)
+                    print("points(x1,y1), (x2,y2)", x1, y1, x2, y2)
+                    cv2.line(gray, (x1,y1), (x2,y2), (0,0,255), 2)
+        cv2.imshow("Line detection", gray)
         key = cv2.waitKey(1) & 0xFF
     elif setting=="c": # Corner detection
-        frame = vs.read()
+        ret, frame = vs.read()
         frame = imutils.resize(frame, width=600)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray_f32 = np.float32(gray)
-        corner = cv2.cornerHarris(gray, 2, 3, 0.04)
-        corner = cv2.dilate(corner,None)
-        corner[corner>0.01*corner.max()]=[0,0,255]
+        corners = cv2.cornerHarris(gray, 2, 3, 0.04)
+        corner = cv2.cornerSubPix(gray, corners, (5,5), (-1,-1), )
+        # corner = cv2.dilate(corner,None)
+        # ret, dst = cv2.threshold(corner, 0.01*corner.max(), 255,0)
+        #
+        # # corner[corner>0.01*corner.max()]=[0,0,255]
         cv2.imshow("Corner Detection", corner)
         key = cv2.waitKey(1) & 0xFF
     elif setting=="d": # Differencing
-        frame0 = vs.read()
+        ret, frame0 = vs.read()
         frame0 = imutils.resize(frame0, width=600)
         gray0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
-        frame1 = vs.read()
+        ret, frame1 = vs.read()
         frame1 = imutils.resize(frame1, width=600)
         gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-        # diff = cv2.absdiff(gray0,gray1)
-        diff = gray0-gray1
+        diff = cv2.absdiff(gray0,gray1)
+        # diff = gray0-gray1
         cv2.imshow("Differencing", diff)
         key = cv2.waitKey(1) & 0xFF
 
 
     if key == ord("q"): # quit
+        vs.release()
         cv2.destroyAllWindows()
         break
     elif key== ord("o"): # ordinary
@@ -77,6 +97,6 @@ while True:
     elif key == ord("d"): # Differencing
         cv2.destroyAllWindows()
         setting="d"
-    elif key == ord("i"): # Invert
+    elif key == ord("l"): # Line detection
         cv2.destroyAllWindows()
-        setting="i"
+        setting="l"
--- hw1/cornerTest.py
+++ hw1/cornerTest.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-filename = 'checkerboard.jpg'
+filename = '../photos/checkerboard.jpg'
 img = cv2.imread(filename)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
