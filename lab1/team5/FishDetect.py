73d48b04f1ad36bde770a2487fe057ce122a49e9
Michael Eyler <eyler94@gmail.com>
2020-01-29 12:30:31 -0700

    Switching to lab computer.

--- lab1/team5/FishDetect.py
+++ lab1/team5/FishDetect.py
@@ -18,16 +18,22 @@ else:
     cap = cv.VideoCapture(args["video"])
 
 sleep(1)
-print("q: quit")#\np: pause")
-# b_total = 0
-# g_total = 0
-# r_total = 0
-# counter = 0
+print("q: quit\np: pause")
 sum0 = 0
 sum1 = 0
 sum2 = 0
 sum3 = 0
 sum4 = 0
+sum5 = 0
+sum6 = 0
+sum7 = 0
+sum8 = 0
+sum9 = 0
+sum10 = 0
+sum11 = 0
+sum12 = 0
+sum13 = 0
+sum14 = 0
 while True:
     _, frame0 = cap.read()
 
@@ -49,11 +55,16 @@ while True:
     g_max = np.max(np.uint32(g))
     r_max = np.max(np.uint32(r))
 
-    # b_total += b_max
-    # g_total += g_max
-    # r_total += r_max
-    # counter+=1
-    # sum_max = b_max+g_max+r_max
+    sum14 = sum13
+    sum13 = sum12
+    sum12 = sum11
+    sum11 = sum10
+    sum10 = sum9
+    sum9 = sum8
+    sum8 = sum7
+    sum7 = sum6
+    sum6 = sum5
+    sum5 = sum4
     sum4 = sum3
     sum3 = sum2
     sum2 = sum1
@@ -62,32 +73,41 @@ while True:
 
     # sum_avg = np.int((sum4+sum3+sum2+sum1+sum0)/5)
     # print(sum_avg)
-    sum_set = np.array([sum0, sum1, sum2])#, sum3, sum4])
-    print("sum_set:", sum_set)
-    # sum_logic = sum_set>=90
-    # print("sum_logic:", sum_logic)
-    # if sum_avg>=90:
-    #     caption = "red"
-    # else:
-    #     caption = "no target"
-    # if sum_avg>=140:
-    #     caption = "green"
-    # if sum_avg>=200:
-    #     caption = "orange"
-    # if sum_avg>=270:
-    #     caption = "yellow"
-    if all(sum_set>=270):
-        caption = "yellow"
-        print(caption)
-    elif all(sum_set>=200):
-        caption = "orange"
-        print(caption)
-    elif all(sum_set>=140):
-        caption = "green"
-        print(caption)
-    elif all(sum_set>=90):
-        caption = "red"
-        print(caption)
+    sum_set = np.array([sum0, sum1, sum2, sum3, sum4, sum5, sum6, sum7, sum8, sum9, sum10, sum11, sum12, sum13, sum14])
+    if sum0 >= 90:
+        # print("sum_set:", sum_set)
+        if all(sum_set>=270):
+            caption = "yellow"
+            # print(caption)
+        elif all(sum_set>=200):
+            caption = "orange"
+            # print(caption)
+        elif all(sum_set>=140):
+            caption = "green"
+            # print(caption)
+        elif all(sum_set>=90):
+            caption = "red"
+            # print(caption)
+        else:
+            caption = "no target"
+            # print(caption)
+    if sum0 >= 90:
+        sum_check = sum9
+        if sum_check>=270:
+            caption = "yellow"
+            print(caption)
+        elif sum_check>=200:
+            caption = "orange"
+            print(caption)
+        elif sum_check>=140:
+            caption = "green"
+            print(caption)
+        elif sum_check>=90:
+            caption = "red"
+            print(caption)
+        else:
+            caption = "calibrating"
+            print(caption)
     else:
         caption = "no target"
         print(caption)
@@ -115,11 +135,4 @@ while True:
         print("Blue max:", np.max(b))
         print("Green max:", np.max(g))
         print("Red max:", np.max(r))
-        # print("Blue average since last pause:", b_total/counter)
-        # print("Green average since last pause:", g_total/counter)
-        # print("Red average since last pause:", r_total/counter)
         _ = input("Press the anykey to continue.")
-        # b_total = 0
-        # g_total = 0
-        # r_total = 0
-        # counter = 0
