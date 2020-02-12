4a22a1fbbf7cac0519c922f202dcfea127f88e17
Michael Eyler <eyler94@gmail.com>
2020-02-12 08:12:18 -0700

    Finished with hw2.

--- hw2/task1.py
+++ hw2/task1.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 import cv2
+import imutils
 
-def disp_check(img = cv2.imread("AR1.jpg"), save_image=False):
+def disp_check(img = cv2.imread("AR1.jpg"), save_image=False, corners_x = 10, corners_y = 7):
+    img = imutils.resize(img, width=600)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    status, corners = cv2.findChessboardCorners(gray, (10,7))
+    status, corners = cv2.findChessboardCorners(gray, (corners_x,corners_y))
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
     corners = cv2.cornerSubPix(gray, corners, (5,5), (-1,-1), criteria)
-    cv2.drawChessboardCorners(img, (10,7), corners, status)
+    cv2.drawChessboardCorners(img, (corners_x,corners_y), corners, status)
 
     frame = img
     print("frame shape:", frame.shape[::-1])
@@ -17,10 +19,11 @@ def disp_check(img = cv2.imread("AR1.jpg"), save_image=False):
     if save_image:
         cv2.imwrite("AR1_rainbow.png",frame)
 
-def ret_corners(img_path):
+def ret_corners(img_path, corners_x=10, corners_y=7):
     img = cv2.imread(img_path)
+    img = imutils.resize(img, width=600)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    status, corners = cv2.findChessboardCorners(gray, (10,7))
+    status, corners = cv2.findChessboardCorners(gray, (corners_x,corners_y))
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
     corners = cv2.cornerSubPix(gray, corners, (5,5), (-1,-1), criteria)
     return corners, img
