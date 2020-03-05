eaea22428e93cf5f470fb3dc05e65f450afd0c86
Michael Eyler <eyler94@gmail.com>
2020-03-04 20:54:43 -0700

    Finished with hw3.

--- hw3/task1.py
+++ hw3/task1.py
@@ -3,65 +3,54 @@ import numpy as np
 import cv2
 import imutils
 
-def disp_check(img = cv2.imread("AR1.jpg"), save_image=False, corners_x = 10, corners_y = 7):
-    img = imutils.resize(img, width=600)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    status, corners = cv2.findChessboardCorners(gray, (corners_x,corners_y))
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-    corners = cv2.cornerSubPix(gray, corners, (5,5), (-1,-1), criteria)
-    cv2.drawChessboardCorners(img, (corners_x,corners_y), corners, status)
-
-    frame = img
-    print("frame shape:", frame.shape[::-1])
-    cv2.imshow("frame", frame)
-    cv2.waitKey(0)
-    if save_image:
-        cv2.imwrite("AR1_rainbow.png",frame)
+search_kernel = 11
+max_iter = 30
 
 def ret_corners(img_path, corners_x=10, corners_y=7):
     img = cv2.imread(img_path)
-    img = imutils.resize(img, width=600)
+    # img = imutils.resize(img, width=600)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    status, corners = cv2.findChessboardCorners(gray, (corners_x,corners_y))
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-    corners = cv2.cornerSubPix(gray, corners, (5,5), (-1,-1), criteria)
+    status, corners = cv2.findChessboardCorners(gray, (corners_x,corners_y), None)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, max_iter, 0.001)
+    corners = cv2.cornerSubPix(gray, corners, (search_kernel,search_kernel), (-1,-1), criteria)
     return corners, img
 
 
-corners_x = 10
-corners_y = 7
+if __name__=="__main__":
 
-image_set = ["L", "R"]
+    corners_x = 10
+    corners_y = 7
 
-for image in image_set:
+    image_set = ["L", "R"]
 
-    list_obj_points = [] # list of point vectors in 3d space
-    list_image_points = [] # list of point vectors in the image plane
+    for image in image_set:
 
-    obj_points = np.zeros((corners_x*corners_y,3), dtype = np.float32) ### Is vec3f float32?
-    obj_points[:,:2] = np.mgrid[0:corners_x,0:corners_y].T.reshape(-1,2)
+        list_obj_points = [] # list of point vectors in 3d space
+        list_image_points = [] # list of point vectors in the image plane
+
+        obj_points = np.zeros((corners_x*corners_y,3), dtype = np.float32)
+        obj_points[:,:2] = np.mgrid[0:corners_x,0:corners_y].T.reshape(-1,2)
 
 
-    for num in range(0,32):
-        img_path = "Camera"+image+str(num)+".bmp"
-        corners, img = ret_corners(img_path)
-        status=True
-        cv2.drawChessboardCorners(img, (corners_x,corners_y), corners, status)
-        cv2.imshow("frame", img)
-        cv2.waitKey(5)
-        list_image_points.append(corners)
-        list_obj_points.append(obj_points)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(list_obj_points, list_image_points, gray.shape[::-1], None, None)
-    print(f"Intrinsic parameters for camera  {image}:\n", cameraMatrix)
-    distCoeffs = distCoeffs.T
-    print(f"Distortion coeffecients for camera {image}:\n", distCoeffs)#, rvecs, tvecs)
+        for num in range(0,32):
+            img_path = image+str(num)+".png"
+            corners, img = ret_corners(img_path)
+            status=True
+            cv2.drawChessboardCorners(img, (corners_x,corners_y), corners, status)
+            cv2.imshow("frame", img)
+            cv2.waitKey(5)
+            list_image_points.append(corners)
+            list_obj_points.append(obj_points)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(list_obj_points, list_image_points, gray.shape[::-1], None, None)
+        print(f"Intrinsic parameters for camera  {image}:\n", cameraMatrix)
+        distCoeffs = distCoeffs.T
+        print(f"Distortion coeffecients for camera {image}:\n", distCoeffs)#, rvecs, tvecs)
 
-    unit_conv = 7.4e-3 ## mm/pix
-    fsx = cameraMatrix[0,0]
-    focal_length = fsx*unit_conv
-    print(f"Focal length for camera {image}:\n", focal_length)
+        unit_conv = 7.4e-3 ## mm/pix
+        fsx = cameraMatrix[0,0]
+        focal_length = fsx*unit_conv
+        print(f"Focal length for camera {image}:\n", focal_length)
 
-    np.save(f'cameraMatrix{image}.npy', cameraMatrix)
-    np.save(f'distCoeffs{image}.npy', distCoeffs)
+        np.save(f'cameraMatrix{image}.npy', cameraMatrix)
+        np.save(f'distCoeffs{image}.npy', distCoeffs)
--- hw3/task3.py
+++ hw3/task3.py
@@ -3,9 +3,12 @@ import numpy as np
 import cv2
 import imutils
 
+search_kernel = 11
+max_iter = 30
+
 def EpiLines(img_name, cameraMatrix, distCoeffs, img_num, F):
-    img = cv2.imread(f"./{img_name}.bmp")
-    img = imutils.resize(img, width=600)
+    img = cv2.imread(f"./{img_name}.png")
+    # img = imutils.resize(img, width=600)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     undist = cv2.undistort(gray, cameraMatrix, distCoeffs)#, None, cameraMatrix)
@@ -14,7 +17,7 @@ def EpiLines(img_name, cameraMatrix, distCoeffs, img_num, F):
     features = cv2.goodFeaturesToTrack(undist, maxCorners=3, qualityLevel=0.1, minDistance=200)
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
     corners = cv2.cornerSubPix(undist, features, (5,5), (-1,-1), criteria)
-    x_lines = [0, 600, 0, 600, 0, 600]
+    x_lines = [-100, 640, -100, 640, -100, 640]
     y_lines = []
     if corners is not None: # if there are corners detected
         for spot, point in enumerate(corners): # why is there a ghost artifact in the first element
@@ -23,17 +26,18 @@ def EpiLines(img_name, cameraMatrix, distCoeffs, img_num, F):
             cv2.circle(img, (x, y), 20, (0, 0, 255), 2)
             lines = cv2.computeCorrespondEpilines(point, img_num, F)
             a,b,c = lines[0][0]
-            y_point = -int(c/b)
+            y_point = -int((c+a*x_lines[spot*2])/b)
             y_lines.append(y_point)
-            y_point = -int((c+x_lines[spot*2+1])/b)
+            y_point = -int((c+a*x_lines[spot*2+1])/b)
             y_lines.append(y_point)
 
     lines = [x_lines, y_lines]
-    cv2.imshow("original", img)
-    cv2.imshow("undistorted", undist)
-    cv2.imshow("difference", diff)
-    cv2.imwrite(f"difference_{img_name}.png",diff)
+    # cv2.imshow("original", img)
+    # cv2.imshow("undistorted", undist)
+    # cv2.imshow("difference", diff)
+    # cv2.imwrite(f"difference_{img_name}.png",diff)
     cv2.waitKey(0)
+    cv2.destroyAllWindows
     return diff, img, lines
 
 if __name__ == "__main__":
@@ -42,13 +46,11 @@ if __name__ == "__main__":
     cameraMatrix_r = np.load('cameraMatrixL.npy')
     distCoeffs_r = np.load('distCoeffsR.npy')
     F = np.load('F.npy')
-    diff_l, img_l, lines_l = EpiLines('StereoL0',cameraMatrix_l, distCoeffs_l,1,F)
-    diff_r, img_r, lines_r = EpiLines('StereoR0',cameraMatrix_r, distCoeffs_r,2,F)
-    print("lines_l:", lines_l)
-    print("lines_r:", lines_r)
+    diff_l, img_l, lines_l = EpiLines('stereo_L1',cameraMatrix_l, distCoeffs_l,1,F)
+    diff_r, img_r, lines_r = EpiLines('stereo_R1',cameraMatrix_r, distCoeffs_r,2,F)
     for spot in range(3):
-        cv2.line(img_l, (lines_r[0][spot*2],lines_r[0][spot*2+1]), (lines_r[1][spot*2],lines_r[1][spot*2+1]), (0,255,0), 2)
-        cv2.line(img_r, (lines_l[0][spot*2],lines_l[0][spot*2+1]), (lines_l[1][spot*2],lines_l[1][spot*2+1]), (0,255,0), 2)
+        cv2.line(img_l, (lines_r[0][spot*2],lines_r[1][spot*2]), (lines_r[0][spot*2+1],lines_r[1][spot*2+1]), (0,255,0), 2)
+        cv2.line(img_r, (lines_l[0][spot*2],lines_l[1][spot*2]), (lines_l[0][spot*2+1],lines_l[1][spot*2+1]), (0,255,0), 2)
     cv2.imshow("left", img_l)
     cv2.imshow("right", img_r)
     cv2.waitKey(0)
