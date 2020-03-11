a45a489aa8a6c54b92c47c2a1ccced5c49adca3f
Michael Eyler <eyler94@gmail.com>
2020-03-11 10:52:16 -0600

    Task 1 is almost done.

---
+++ hw4/task1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import numpy as np
+import cv2
+import imutils
+
+search_kernel = 11
+max_iter = 30
+
+def ret_corners(img_path, corners_x=10, corners_y=7):
+    img = cv2.imread(img_path)
+    # img = imutils.resize(img, width=600)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    status, corners = cv2.findChessboardCorners(gray, (corners_x,corners_y), None)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, max_iter, 0.001)
+    corners = cv2.cornerSubPix(gray, corners, (search_kernel,search_kernel), (-1,-1), criteria)
+    return corners, img
+
+if __name__=="__main__":
+
+    corners_x = 10
+    corners_y = 7
+
+    img_path = "stereo_L1.png"
+    corners_l, img_l = ret_corners(img_path)
+    cameraMatrix_l = np.load('cameraMatrixL.npy')
+    distCoeffs_l = np.load('distCoeffsL.npy')
+    R1 = np.load('R1.npy')
+    P1 = np.load('P1.npy')
+    status=True
+    special_corners = [0, 9, -10, -1]
+    # print("corners:\n", corners_l[special_corners])
+    corners_undistorted_l = cv2.undistortPoints(corners_l[special_corners], cameraMatrix_l, distCoeffs_l, R=R1, P=P1)
+    # print("corners_undistorted_l:\n", corners_undistorted_l)
+    cv2.drawChessboardCorners(img_l, (2,2), corners_l[special_corners], status)
+    cv2.imshow("frame_l", img_l)
+
+    img_path = "stereo_R1.png"
+    corners_r, img_r = ret_corners(img_path)
+    cameraMatrix_r = np.load('cameraMatrixR.npy')
+    distCoeffs_r = np.load('distCoeffsR.npy')
+    R2 = np.load('R2.npy')
+    P2 = np.load('P2.npy')
+    # print("corners:\n", corners_r[special_corners])
+    corners_undistorted_r = cv2.undistortPoints(corners_r[special_corners], cameraMatrix_r, distCoeffs_r, R=R2, P=P2)
+    # print("corners_undistorted_r:\n", corners_undistorted_r)
+    cv2.drawChessboardCorners(img_r, (2,2), corners_r[special_corners], status)
+    cv2.imshow("frame_r", img_r)
+
+    points = np.zeros((4,1,3))
+    for row_iter, col in enumerate(points):
+        col[0,0] = corners_undistorted_l[row_iter][0][0]
+        col[0,1] = corners_undistorted_l[row_iter][0][1]
+        col[0,2] = corners_undistorted_l[row_iter][0][0]-corners_undistorted_r[row_iter][0][0]
+    # points = points.T
+    print("points:\n", points)
+    Q = np.load('Q.npy')
+    points_3d = cv2.perspectiveTransform(points,Q)
+    print("3d points:", points_3d)
+
+    cv2.waitKey(0)
--- hw4/task4.py
+++ hw4/task4.py
@@ -6,8 +6,8 @@ import imutils
 
 if __name__ == "__main__":
     cameraMatrix_l = np.load('cameraMatrixL.npy')
-    distCoeffs_l = np.load('distCoeffsR.npy')
-    cameraMatrix_r = np.load('cameraMatrixL.npy')
+    distCoeffs_l = np.load('distCoeffsL.npy')
+    cameraMatrix_r = np.load('cameraMatrixR.npy')
     distCoeffs_r = np.load('distCoeffsR.npy')
     R = np.load('R.npy')
     T = np.load('T.npy')
@@ -17,6 +17,15 @@ if __name__ == "__main__":
     print("P1:", P1)
     print("P2:", P2)
     print("Q:", Q)
+    Q = np.save('Q.npy', Q)
+    R1 = np.save('R1.npy',R1)
+    R2 = np.save('R2.npy',R2)
+    P1 = np.save('P1.npy',P1)
+    P2 = np.save('P2.npy',P2)
+
+
+
+    # mp1_l, mp2_l = cv2.initUndistortRectifyMap(cameraMatrix_l, distCoeffs_l, R1, P1, (640,480), cv2.CV_32FC2)
     mp1_l, mp2_l = cv2.initUndistortRectifyMap(cameraMatrix_l, distCoeffs_l, R1, P1, (640,480), cv2.CV_32FC2)
     img_l = cv2.imread('stereo_L1.png')
     cv2.imshow("orig left", img_l)
@@ -24,13 +33,17 @@ if __name__ == "__main__":
     for ii in range(1,7):
         cv2.line(new_l,(0,int(480*ii/7)),(640,int(480*ii/7)),(0,255,0),2)
     cv2.imshow("new left", new_l)
-    mp1_r, mp2_r = cv2.initUndistortRectifyMap(cameraMatrix_l, distCoeffs_l, R1, P1, (640,480), cv2.CV_32FC2)
+
+
+    # mp1_r, mp2_r = cv2.initUndistortRectifyMap(cameraMatrix_l, distCoeffs_l, R1, P1, (640,480), cv2.CV_32FC2)
+    mp1_r, mp2_r = cv2.initUndistortRectifyMap(cameraMatrix_r, distCoeffs_r, R2, P2, (640,480), cv2.CV_32FC2)
     img_r = cv2.imread('stereo_R1.png')
     cv2.imshow("orig right", img_r)
     new_r = cv2.remap(img_r, mp1_r, mp2_r,cv2.INTER_NEAREST)
     for ii in range(1,7):
         cv2.line(new_r,(0,int(480*ii/7)),(640,int(480*ii/7)),(0,255,0),2)
     cv2.imshow("new right", new_r)
+
 
     diff_l = cv2.absdiff(img_l,new_l)
     diff_r = cv2.absdiff(img_r,new_r)
