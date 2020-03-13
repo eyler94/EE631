2590a341c10866cfec6a5e768360a57386f8b47c
Michael Eyler <eyler94@gmail.com>
2020-03-12 22:49:04 -0600

    Finished with hw4.

--- hw4/task3.py
+++ hw4/task3.py
@@ -14,17 +14,20 @@ kernel = np.ones((5,5))
 counter = 0
 found_ball = False
 
-def ret_img_and_ball_centroids(img, xl, xh, yl, yh):
+def ret_img_and_ball_centroids(img, xl, xh, yl, yh,side):
+    image_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = img.copy()
     cv2.rectangle(img,(xl,yl),(xh,yh),(0,255,0),2)
-    image_cropped = img[yl:yh, xl:xh]
-    image_gray = cv2.cvtColor(image_cropped, cv2.COLOR_BGR2GRAY)
-    _, image_thresh = cv2.threshold(image_gray, 50, 255, cv2.THRESH_BINARY)
-    image_erode = cv2.erode(image_thresh, kernel, iterations = 2)
-    image_dilate = cv2.dilate(image_erode, kernel, iterations = 2)
-
-    contours, _  = cv2.findContours(image_dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    image_cropped = image_gray[yl:yh, xl:xh]
+    _, image_thresh = cv2.threshold(image_cropped, 60, 255, cv2.THRESH_BINARY)
+    image_erode = cv2.erode(image_thresh, kernel, iterations = 1)
+    image_dilate = cv2.dilate(image_erode, kernel, 2)
+    cv2.imshow(f"dilate{side}", image_dilate)
+    contours, _  = cv2.findContours(image_dilate.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # contours = imutils.grab_contours(contours)
     cent = np.array([0,0])
     if len(contours) > 0:
+        print("contours:", contours)
         max_contour = max(contours,key=cv2.contourArea)
         Image_moment = cv2.moments(max_contour)
         x_center_m = int(Image_moment['m10']/Image_moment['m00'])
@@ -52,18 +55,18 @@ _, frame_r = cap_r.read()
 
 while frame_l is not None:
     _, frame_l = cap_l.read()
-    frame_l, cent_l = ret_img_and_ball_centroids(frame_l, 295, 295+130, 20, 170)
-    # print("left cent:", cent_l)
+    frame_l, cent_l = ret_img_and_ball_centroids(frame_l, 295, 295+130, 20, 170,"l")
+    print("left cent:", cent_l)
     cv2.imshow("left:", frame_l)
 
     _, frame_r = cap_r.read()
-    frame_r, cent_r = ret_img_and_ball_centroids(frame_r, 220, 350, 20, 170)
-    # print("right cent:", cent_r)
+    frame_r, cent_r = ret_img_and_ball_centroids(frame_r, 212, 212+130, 20, 170,"r")
+    print("right cent:", cent_r)
     cv2.imshow("right:", frame_r)
 
     if cent_l[0] == 0 or cent_r[0] == 0:
         key = cv2.waitKey(1)
-        if counter>15:
+        if counter>20:
             print("3d points:", points_3d)
             np.save('points_3d.npy',points_3d)
             break
--- hw4/task3b.py
+++ hw4/task3b.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+# from numpy.polynomial import polynomial as polyfit
 
 points_3d = np.load('points_3d.npy')
 
@@ -12,21 +13,39 @@ x_points = points_catcher.T[0][0]
 z_points = points_catcher.T[2][0]
 legend_str = []
 time = 0
+
+coef = np.polyfit(z_points, x_points, 2)
+x_func = np.poly1d(coef)
+t = np.linspace(z_points[0],0,100)
+x = x_func(t)
+
 for point_z, point_x in zip(z_points,x_points):
     plt.plot(point_z,point_x,'o')
     legend_str.append(f"time: {time}")
     time +=1
+plt.plot(t,x)
 plt.legend(legend_str)
+plt.ylabel("x")
+plt.xlabel("z")
 plt.title("z vs. x")
 
 fig2 = plt.figure(2)
 y_points = points_catcher.T[1][0]
 legend_str = []
 time = 0
+
+coef = np.polyfit(z_points, y_points, 2)
+y_func = np.poly1d(coef)
+t = np.linspace(z_points[0],0,100)
+y = y_func(t)
+
 for point_z, point_y in zip(z_points,y_points):
     plt.plot(point_z,point_y,'o')
     legend_str.append(f"time: {time}")
     time +=1
+plt.plot(t,y)
 plt.legend(legend_str)
+plt.ylabel("y")
+plt.xlabel("z")
 plt.title("z vs. y")
 plt.show()
